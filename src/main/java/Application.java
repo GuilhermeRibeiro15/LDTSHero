5870d785c2dd751136f628bb26e0d4f3174c6137
Guilherme Ribeiro <up202108731@up.pt>
2022-09-27 10:22:37 +0100

    Third version

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,40 @@
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
+
 public class Application {
-public static void main(String[] args) {
-System.out.println("Hello World!");
+public static void main(String[] args) throws IOException {
+ Terminal terminal = new
+         DefaultTerminalFactory().createTerminal();
+ Screen screen = new TerminalScreen(terminal);
+ screen.setCursorPosition(null); // we don't need a cursor
+ screen.startScreen(); // screens must be started
+ screen.doResizeIfNecessary(); // resize screen if necessary
+ try {
+  terminal = new
+          DefaultTerminalFactory().createTerminal();
+  screen = new TerminalScreen(terminal);
+  screen.setCursorPosition(null); // we don't need a cursor
+  screen.startScreen(); // screens must be started
+  screen.doResizeIfNecessary(); // resize screen if
+  Object necessary = null;
+  Object necessary1 = null;
+ } catch (IOException e) {
+  e.printStackTrace();
+ }
+ TerminalSize terminalSize = new TerminalSize(40, 20);
+ DefaultTerminalFactory terminalFactory = new
+         DefaultTerminalFactory()
+         .setInitialTerminalSize(terminalSize);
+ terminal = terminalFactory.createTerminal();
+ screen.clear();
+ screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')
+         [0]);
+ screen.refresh();
  }
 }
