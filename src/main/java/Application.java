3a1d39351b2964bb5e54c79bb318871e07a13197
Guilherme Ribeiro <up202108731@up.pt>
2022-10-04 09:14:08 +0100

    Version 1 04/10

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -9,33 +9,8 @@ import java.io.IOException;
 
 public class Application {
 public static void main(String[] args) throws IOException {
- Terminal terminal = new
-         DefaultTerminalFactory().createTerminal();
- Screen screen = new TerminalScreen(terminal);
- screen.setCursorPosition(null); // we don't need a cursor
- screen.startScreen(); // screens must be started
- screen.doResizeIfNecessary(); // resize screen if necessary
- try {
-  terminal = new
-          DefaultTerminalFactory().createTerminal();
-  screen = new TerminalScreen(terminal);
-  screen.setCursorPosition(null); // we don't need a cursor
-  screen.startScreen(); // screens must be started
-  screen.doResizeIfNecessary(); // resize screen if
-  Object necessary = null;
-  Object necessary1 = null;
- } catch (IOException e) {
-  e.printStackTrace();
- }
- TerminalSize terminalSize = new TerminalSize(40, 20);
- DefaultTerminalFactory terminalFactory = new
-         DefaultTerminalFactory()
-         .setInitialTerminalSize(terminalSize);
- terminal = terminalFactory.createTerminal();
- screen.clear();
- screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')
-         [0]);
- screen.refresh();
- }
+  Game game = new Game();
+  game.run();
+}
 }
 
